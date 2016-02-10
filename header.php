dcece139b88fdb71da7a65444b1486909ac86205
Nazar <nazar_peleh@meta.ua>
2016-02-10 09:14:24 +0200

    WP Theme

--- header.php
+++ header.php
@@ -19,7 +19,7 @@ ob_start('ob_html_compress');
     <meta name="HandheldFriendly" content="True"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
     <meta name="apple-mobile-web-app-capable" content="yes"/>
-    <meta name="theme-color" content="#6aa35b">//add color
+    <meta name="theme-color" content="#6aa35b"><!--add color-->
     <link rel="shortcut icon" href="<?php theme(); ?>/favicon.ico" type="image/x-icon"/>
     <?php wp_head(); ?>
 </head>
