995c7be3c0efb596921ab3810311f609421441fc
Nazar <nazar_peleh@meta.ua>
2016-02-26 15:17:05 +0200

    Update init.js
    Contact Form 7 Validation

--- js/init.js
+++ js/init.js
@@ -1,5 +1,13 @@
 $ = jQuery;
 $(document).ready(function () {
+    
+    //  contact form 7
+    $(this).on('click', '.wpcf7-not-valid-tip', function(){
+        $(this).prev().trigger('focus');
+        $(this).fadeOut(500,function(){
+            $(this).remove();
+        });
+    });
 
     /* Swiper Slider */
     var swiper = new Swiper('.swiper-container', {
