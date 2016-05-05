b793cc4532553d185cb4f733279488895376a184
Nazar <nazar_peleh@meta.ua>
2016-05-05 12:03:17 +0300

    Change title

--- header.php
+++ header.php
@@ -15,7 +15,7 @@ ob_start('ob_html_compress');
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-    <title><?php seo_title(); ?></title>
+    <title><?php wpa_title(); ?></title>
     <meta name="HandheldFriendly" content="True"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
     <meta name="apple-mobile-web-app-capable" content="yes"/>
