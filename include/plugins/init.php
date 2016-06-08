56a01866676c3003d6d577d7732ee220842f3d01
Nazar <nazar_peleh@meta.ua>
2016-06-08 10:28:41 +0300

    qTranslate Fix
    Now can change menu links also

--- include/plugins/init.php
+++ include/plugins/init.php
@@ -265,3 +265,44 @@ function my_theme_register_required_plugins()
     );
     tgmpa($plugins, $config);
 }
+
+/* ===== qTranslate/qTranslate X  ===== */
+if(defined('QTX_VERSION')) {
+    remove_action('wp_head', 'qtranxf_wp_head_meta_generator');
+    remove_action('wp_head', 'qtranxf_head', 10, 0);
+    remove_action('wp_head', 'qtranxf_wp_head', 10, 0);
+    remove_action('wp_head', 'qtrans_header', 10, 0);
+    // Convert blogurl
+    function qtrans_home_url($url = '') {
+        return qtranxf_convertURL(site_url($url));
+    }
+    // Custom Links fix
+    add_filter('walker_nav_menu_start_el', 'qtrans_in_nav_el', 10, 4);
+    function qtrans_in_nav_el($item_output, $item, $depth, $args){
+        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+        /*
+            Example link: http://google.com|en|http://google.com.ua|ua|
+        */
+        if(preg_match_all('~(.*?)\|(\w{2,})\|~', $item->url, $matches)) {
+            $ext_url = '';
+            foreach ($matches[1] as $i => $match) {
+                $ext_url .= "[:{$matches[2][$i]}]$match";
+            }
+            $item->url = esc_attr( __( $ext_url ) );
+        }
+        // Determine integration with qTranslate Plugin
+        if (function_exists('qtranxf_convertURL')) {
+            $attributes .= ! empty( $item->url ) ? ' href="' . qtranxf_convertURL(esc_attr( $item->url )) .'"' : '';
+        } else {
+            $attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) .'"' : '';
+        }
+        $item_output = $args->before;
+        $item_output .= '<a'. $attributes .'>';
+        $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+        return $item_output;
+    }
+}
