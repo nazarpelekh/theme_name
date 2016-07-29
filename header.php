d73dd42c7fb40b11a0b7e35bc330c2eada03aea5
Nazar <nazar_peleh@meta.ua>
2016-07-29 11:48:53 +0300

    Update header.php

--- header.php
+++ header.php
@@ -20,6 +20,7 @@ ob_start('ob_html_compress');
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0"/>
     <meta name="apple-mobile-web-app-capable" content="yes"/>
     <meta name="theme-color" content="#6aa35b"><!--add color-->
+    <meta name="format-detection" content="telephone=no">
     <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="<?php echo theme('images/favicon.png'); ?>" sizes="16x16 32x32 48x48">
     <link rel="shortcut icon" type="image/x-icon" href="<?php echo theme('images/favicon.png'); ?>" sizes="16x16">
     <link rel="icon" type="image/x-icon" href="<?php echo theme('images/favicon.png'); ?>" sizes="16x16">
